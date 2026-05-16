867aebeca9053af10e5a5b61054daf3a6fbd9fa2
LeilaAyman <mariamaymanaliomar@gmail.com>
2026-05-16 19:07:05 +0300

    routes /controllers

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,14 @@ const {DynamoDBDocumentClient, PutCommand, GetCommand, DeleteCommand} = require(
 const client = new DynamoDBClient({region: "eu-north-1"});
 const dynamo = DynamoDBDocumentClient.from(client);
 
+const tasksRoutes = require("./src/routes/tasks");
 
 const app = express();
 app.use(express.json());
+
+// Register routes
+app.use("/api/tasks", tasksRoutes);
+
 const port = 4000;
 app.listen(port,()=>{
     console.log(`Server running on ${port}`)
